6be50fdb7926f33292fa98d81f17cead44fd803d
Dennis Mwaniki <damos5776@gmail.com>
2021-07-24 15:18:41 +0300

    service add

---
+++ src/app/service/service.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+import {map} from 'rxjs/operators'
+import {Observable} from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+username:string
+  constructor(private http:HttpClient) {
+    console.log('service is now ready')
+    this.username='mwaniki9322'
+   }
+   getPersonsInfo(){
+    return this.http.get(`https://api.github.com/users/${this.username}?access_token?client_id= ${environment.acessToken}`)
+}
+getPersonsRepos(){
+  return this.http.get(`https://api.github.com/users/Alice-Githui/repos?access_token?client_id= ${environment.acessToken}`)
+}
+updateProfile(username: string){
+  this.username = username
+
+}
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  acessToken:'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'
 };
 
 /*
