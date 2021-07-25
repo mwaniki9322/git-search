2236ca54ba0ae867414062eff385c4d687883fb5
Dennis Mwaniki <damos5776@gmail.com>
2021-07-25 12:46:58 +0300

    homepage styling

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,5 @@
 export class User {
     constructor(public login: string, public id: number, public avatar_url: string, 
-        public created_at: string,public followers:number,public following:number){
+        public created_at: string,public followers:number,public following:number,public public_repos:number){
     }
 }
