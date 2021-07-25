88e91f91e02dd8893bb3424c256dbd91de6a5285
Dennis Mwaniki <damos5776@gmail.com>
2021-07-25 13:54:20 +0300

    repo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomepageComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
