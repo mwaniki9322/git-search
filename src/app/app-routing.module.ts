c9f8a6052bab247f658eb8d805d4571955b555c3
Dennis Mwaniki <damos5776@gmail.com>
2021-07-25 10:54:43 +0300

    router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:LandingpageComponent},
+  {path:'finduser',component:HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
